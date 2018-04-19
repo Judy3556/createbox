524d536ee2249585ebeadcfbae627b8bb35d78ed
m305-14\w401 <gfgfdhgf@163.com>
2018-04-19 16:18:27 +0800

    注解

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,18 +10,26 @@ namespace createbox
     {
         static void Main(string[] args)
         {
+            //问个好
             Console.WriteLine("hello");
+
+            //定义文件夹位子
             string dirPath = @"C:\47";
 
+            //如果文件夹存在，销毁文件夹
             if (Directory.Exists(dirPath))
             {
                 Console.WriteLine("The directory {0} already exists.", dirPath);
             }
+
+            //如果文件夹不存在，建立新文件夹
             else
             {
                 Directory.CreateDirectory(dirPath);
                 Console.WriteLine("The directory {0} was created.", dirPath);
             }
+
+
             Console.Read();
         }
     }
