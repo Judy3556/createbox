9a015d0056abf85983e6fd2d9086e993491c4d17
m305-14\w401 <gfgfdhgf@163.com>
2018-04-19 16:11:39 +0800

    开号重练

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace createbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+            string dirPath = @"C:\47";
+
+            if (Directory.Exists(dirPath))
+            {
+                Console.WriteLine("The directory {0} already exists.", dirPath);
+            }
+            else
+            {
+                Directory.CreateDirectory(dirPath);
+                Console.WriteLine("The directory {0} was created.", dirPath);
+            }
+            Console.Read();
+        }
+    }
+}
