3adc40e145aaa2015bf3445b14e91b45a5b2f96e
LAPTOP-IABSIHS6\pc-01 <1147663235@qq.com>
2018-05-14 11:29:00 +0800

    修改為創建多個文件夾并進行美化

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,26 +11,39 @@ namespace createbox
         static void Main(string[] args)
         {
             //问个好
-            Console.WriteLine("hello");
+            Console.WriteLine();
+            Console.WriteLine(" 歡迎您使用本文件夾創建器");
+            Console.WriteLine(" ");
 
-            //定义文件夹位子
-            string dirPath = @"C:\47";
+            //確定建立文件夾的數量
+            Console.WriteLine(" 請問您要建立幾個文件夾？");
+            int Count = int.Parse(Console.ReadLine());
 
-            //如果文件夹存在，销毁文件夹
-            if (Directory.Exists(dirPath))
+            //創建多個文件夾
+            for (int i = 0; i < Count; i++)
             {
-                Console.WriteLine("The directory {0} already exists.", dirPath);
+                int j = i + 1;
+                Console.WriteLine(" ——————————————————————");
+                //定义文件夹位子
+                string dirPath = @"C:\NewWork"+"("+j+")";
+
+                //如果文件夹存在，销毁文件夹
+                if (Directory.Exists(dirPath))
+                {
+                    Console.WriteLine(" The directory {0} already exists.", dirPath);
+                }
+
+                //如果文件夹不存在，建立新文件夹
+                else
+                {
+                    Directory.CreateDirectory(dirPath);
+                    Console.WriteLine(" The directory {0} was created.", dirPath);
+                }
             }
 
-            //如果文件夹不存在，建立新文件夹
-            else
-            {
-                Directory.CreateDirectory(dirPath);
-                Console.WriteLine("The directory {0} was created.", dirPath);
-            }
-
-
-            Console.Read();
+            Console.WriteLine(" ——————————————————————");
+            Console.WriteLine(" 感謝您的使用！");
+            Console.ReadKey();
         }
     }
 }
